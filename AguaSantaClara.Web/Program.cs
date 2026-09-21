de1a5b2cfbd608b5a2fc19761972a12b323ce1df
German R. <germanramosterreros@gmail.com>
2026-09-21 16:41:05 -0500

    T37: Registrar servicios con DI en Program.cs

--- AguaSantaClara.Web/Program.cs
+++ AguaSantaClara.Web/Program.cs
@@ -1,13 +1,34 @@
 using AguaSantaClara.Web.Data;
+using AguaSantaClara.Web.Models.Entities;
+using AguaSantaClara.Web.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// EF Core + PostgreSQL
+// EF Core
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Identity con roles
+builder.Services.AddIdentity<Usuario, Rol>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+// MVC
 builder.Services.AddControllersWithViews();
+
+// Repositorios
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+// Services (los específicos se agregan cuando existan)
+// builder.Services.AddScoped<IProductoService, ProductoService>();
 
 var app = builder.Build();
 
@@ -27,6 +48,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
