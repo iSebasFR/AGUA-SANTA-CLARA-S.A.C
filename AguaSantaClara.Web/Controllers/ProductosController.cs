4a32d8f11d9bb64b2d74ecfa069edbccefee2269
jair quinto <giovanni.quinto02@gmail.com>
2026-09-23 07:59:12 -0500

    T16: Mostrar catalogo de productos

--- AguaSantaClara.Web/Controllers/ProductosController.cs
+++ AguaSantaClara.Web/Controllers/ProductosController.cs
@@ -1,8 +1,26 @@
+using AguaSantaClara.Web.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AguaSantaClara.Web.Controllers;
 
+[Authorize(Roles = "Administradora")]
 public class ProductosController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly AppDbContext _context;
+
+    public ProductosController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var productos = await _context.Productos
+            .OrderBy(p => p.Nombre)
+            .ToListAsync();
+
+        return View(productos);
+    }
 }
